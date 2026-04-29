2f73b4c49b7fa1034221f1f69805d595b9d03b45
Sabene9 <aronasabene.diatta@gmail.com>
2026-04-29 10:17:33 +0000

    file update

--- plot1.R
+++ plot1.R
@@ -2,16 +2,17 @@ Electric_power_consumption <-
   read.csv("household_power_consumption.txt", 
            header = TRUE, sep = ";", dec = ".", na.strings = "?")
 
-Electric_power_consumption$DateTime <- strptime(
-  paste(Electric_power_consumption$Date, Electric_power_consumption$Time),
-  format = "%d/%m/%Y %H:%M:%S"
-)
+Electric_power_consumption$Date <- as.Date(Electric_power_consumption$Date, "%d/%m/%Y")
+
+subset <- Electric_power_consumption[Electric_power_consumption$Date == "2007-02-01" |
+                                       Electric_power_consumption$Date == "2007-02-02", ]
+
+subset$Datetime <- strptime(paste(subset$Date, subset$Time),
+                            "%Y-%m-%d %H:%M:%S")
 
 png("plot1.png", width = 480, height = 480)
-# Exemple de filtrage
-data_subset <- subset(Electric_power_consumption, Date %in% c("1/2/2007", "2/2/2007"))
 
-hist(as.numeric(data_subset$Global_active_power),
+hist(as.numeric(subset$Global_active_power),
      col = "red",
      main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)",
